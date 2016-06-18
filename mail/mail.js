079270e78d67619393f287256b5346e00d743b09
moran sharabi <moransh4@gmail.com>
2016-06-18 11:03:02 +0300

    update

--- mail/mail.js
+++ mail/mail.js
@@ -1,28 +1,28 @@
 
-var express = require('express');
-var router = express.Router();
-var mandrill = requirejs('node-mandrill')('Yg0fG6fsBGN60AiFnCvCOA');
+// var express = require('express');
+// var router = express.Router();
+// var mandrill = requirejs('node-mandrill')('Yg0fG6fsBGN60AiFnCvCOA');
 
 
-    $("#contactForm").submit(function(){
-       alert("heyyyy");
-        //send an e-mail to jim rubenstein
-        mandrill('/messages/send', {
-            message: {
-                to: [{email: 'moransh4@gmail.com', name: 'moran'}],
-                from_email: 'moransh4@gmail.com',
-                subject: "Hey, what's up?",
-                text: "Hello, I sent this message using mandrill."
-            }
-        }, function(error, response)
-        {
-            //uh oh, there was an error
-            if (error) console.log( JSON.stringify(error) );
+//     $("#contactForm").submit(function(){
+//        alert("heyyyy");
+//         //send an e-mail to jim rubenstein
+//         mandrill('/messages/send', {
+//             message: {
+//                 to: [{email: 'moransh4@gmail.com', name: 'moran'}],
+//                 from_email: 'moransh4@gmail.com',
+//                 subject: "Hey, what's up?",
+//                 text: "Hello, I sent this message using mandrill."
+//             }
+//         }, function(error, response)
+//         {
+//             //uh oh, there was an error
+//             if (error) console.log( JSON.stringify(error) );
 
-            //everything's good, lets see what mandrill said
-            else console.log(response);
-        });
-    });
+//             //everything's good, lets see what mandrill said
+//             else console.log(response);
+//         });
+//     });
 
 
 
